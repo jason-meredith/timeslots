993ff29d26eb2d400afe3349a40b2bc9884a7fe8
Jason Meredith <jason-meredith@live.com>
2018-07-04 12:10:37 -0400

    API Namespace -Added the API namespace -Scope schedule, tasks

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,44 @@ Rails.application.routes.draw do
   resources :external_calendars
 
 
+  get 'schedule', to: 'schedule#index'
+
+  post 'schedule/add', to: 'schedule#schedule_task'
+
+
+  namespace :api do
+    scope '/schedule' do
+
+      get 'events(/:week_offset)',
+          to: 'schedule#external_events',
+          defaults: { week_offset: 0},
+          constraints: { week_offset: /[\-]?\d/ }
+
+      get 'scheduled_tasks(/:week_offset)',
+          to: 'schedule#scheduled_tasks',
+          defaults: { week_offset: 0},
+          constraints: { week_offset: /[\-]?\d/ }
+
+      get 'freetime(/:week_offset)',
+          to: 'schedule#freetime',
+          defaults: { week_offset: 0},
+          constraints: { week_offset: /[\-]?\d/ }
+
+      get 'ical', to: 'schedule#generated_calendar'
+
+    end
+
+    scope '/tasks' do
+
+      get 'queued(/:week_offset)',
+          to: 'tasks#queued',
+          defaults: {week_offset: 0},
+          constraints: { week_offset: /[\-]?\d/ }
+
+
+
+    end
+
+  end
+
 end
