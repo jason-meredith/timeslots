90b0a84f3e6f7ad919b6a077d78fcd5a67872f73
Jason Meredith <jason-meredith@live.com>
2018-08-05 14:20:50 -0400

    WeekViewable Concern -DRYed model code with WeekViewable concern

---
+++ app/models/concerns/week_viewable.rb
@@ -0,0 +1,40 @@
+module Week_Viewable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def week(week_offset=0)
+      time_period_start = Time.now.beginning_of_day + ( week_offset * 7.day )
+
+      week_events = []
+
+      all_week_events = self.where(start: time_period_start..time_period_start + 7.day)
+
+      7.times do |weekday|
+        day = time_period_start + weekday.day
+        day_events = {
+            date: {
+                dateNum: day.day,
+                monthNum: day.month,
+                weekDayStr: day.strftime('%a'),
+                monthStr: day.strftime('%^b')
+            }
+        }
+        day_events['events'] = []
+        all_week_events.where(start: time_period_start + weekday.day..time_period_start.end_of_day + weekday.day).each do |event|
+
+          output = yield event
+          day_events['events'] << output
+
+        end
+        week_events << day_events
+      end
+
+      return week_events
+
+    end
+
+
+  end
+
+end
